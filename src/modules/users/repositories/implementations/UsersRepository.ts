6d4e5cbc5f1e59f11f02bde7760926e76de2a07c
Marlon Leoner <marlon.leoner@gmail.com>
2021-08-31 18:54:27 -0300

    challenge

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,40 @@
+import { getRepository, Repository } from "typeorm";
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne(user_id, {
+      relations: ["games"],
+    });
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(
+      "SELECT * FROM users WHERE (lower(first_name) = $1 AND lower(last_name) = $2)",
+      [first_name.toLowerCase(), last_name.toLowerCase()]
+    ); // Complete usando raw query
+  }
+}
